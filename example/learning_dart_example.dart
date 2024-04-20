60e9df5fcf63280561ed91076310a6e8d8294460
shaha <shohruh300908@gmail.com>
2024-04-20 12:34:27 +0500

    version 1

---
+++ example/learning_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:learning_dart/learning_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/learning_dart.dart
@@ -0,0 +1,19 @@
+library;
+
+export 'src/learning_dart_base.dart';
+
+void main() {
+  // final listOfChar = [1, 2, 3, 4, 5, 6, 7];
+
+  // var i = 0;
+  // for (var element in listOfChar) {
+  //   if (listOfChar[element] % 2 == 0) {
+  //     listOfChar[element] *= 2;
+  //     print(listOfChar[element]);
+  //   }
+  // }
+
+  final listOfInt = const [2, 4, 5, 9, 9, 9];
+  final fff = listOfInt.asMap();
+  print(fff);
+}
